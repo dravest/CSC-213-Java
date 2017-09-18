bba07ad6f55441be9d860be34205c909a3452395
Your Name <dravest@my.canisius.edu>
2017-09-17 20:59:25 -0400

    /commit for the animals class after being tested

---
+++ Lab3/src/animals/LionImpl.java
@@ -0,0 +1,21 @@
+package animals;
+
+public class LionImpl implements Lion
+{
+	public void eat()
+	{
+		System.out.println( "I'm a lion and I eat meat");
+	}
+	public void sleep()
+	{
+		System.out.println("I'm a lion and I sleep all day long");
+	}	
+	public void roar()
+	{
+		System.out.println("I'm a lion and HEAR ME ROAR");
+	}
+	public void beTheKing()
+	{
+		System.out.println("I'm a lion and I'm king of the jungle");
+	}
+}
---
+++ Lab3/src/animals/PenguinImpl.java
@@ -0,0 +1,17 @@
+package animals;
+
+public class PenguinImpl implements Penguin
+{
+	public void eat()
+	{
+		System.out.println("I'm a penguin and I eat fish");
+	}
+	public void sleep()
+	{
+		System.out.println("I'm a penguin and I sleep in the cold");
+	}
+	public void waddle()
+	{
+		System.out.println("I'm a penguin I waddle around in the cold");
+	}
+}
