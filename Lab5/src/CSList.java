2a972bab9e0a4d5c83a0fdf5f6065b7525f9d734
Your Name <dravest@my.canisius.edu>
2017-09-30 17:43:53 -0400

    /the begining of the CSList class

---
+++ Lab5/src/CSList.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.util.Comparator;
+
+public class CSList extends ICSList<E>
+{	
+	private CSList head;
+	private CSList tail;
+	private CSList list;
+	
+	public CSList(CSList head, CSList tail, ArrayList list)
+	{
+		Super();
+		list = new ArrayList();
+		this.head = null;
+		this.tail = null;
+	}
+	public int size()
+	{
+		
+	}
+	public boolean isEmpty()
+	{
+		
+	}
+	public boolean contains( E o )
+	{
+		
+	}
+	public boolean add(E e)
+	{
+		
+	}
+	public boolean remove(E o)
+	{
+		
+	}
+	public void clear()
+	{
+		
+	}
+	public E get(int index)
+	{
+		
+	}
+	public E set(int index, E element)
+	{
+		
+	}
+	public void add(int index, E element)
+	{
+		
+	}
+	public E remove(int index)
+	{
+		
+	}
+	public int indexOf(E o)
+	{
+		
+	}
+	public void sort(Comparator<E> comparator)
+	{
+		
+	}
+}
